0ec175c5250b0e7037e80efff9ac9a9cd0264d83
Яблонская Анна Владимировна <AVlYablonskaya@sberbank.ru>
2022-04-25 12:42:19 +0300

    #5 replaces ResponceDto instance with jsonified dto in Routing

--- src/main/kotlin/gosha/kalosha/routing/Routing.kt
+++ src/main/kotlin/gosha/kalosha/routing/Routing.kt
@@ -10,18 +10,16 @@ import io.ktor.routing.*
 import org.koin.ktor.ext.inject
 
 fun Routing.configureRouting() {
-
     val appStatus by inject<AppStatus>()
 
-    val successResponse = ResponseDto(ResponseStatus.SUCCESS, HttpStatusCode.OK, appStatus.namespace)
-
-    val errorResponse = ResponseDto(ResponseStatus.ERROR, HttpStatusCode.InternalServerError, appStatus.namespace)
+    val successResponse = ResponseDto(ResponseStatus.SUCCESS, HttpStatusCode.OK, appStatus.namespace).toJson()
+    val errorResponse = ResponseDto(ResponseStatus.ERROR, HttpStatusCode.InternalServerError, appStatus.namespace).toJson()
 
     get("health") {
         if (appStatus.isOk.get()) {
-            call.respondText(successResponse.toJson(), ContentType.Application.Json, HttpStatusCode.OK)
+            call.respondText(successResponse, ContentType.Application.Json, HttpStatusCode.OK)
         } else {
-            call.respondText(errorResponse.toJson(), ContentType.Application.Json, HttpStatusCode.InternalServerError)
+            call.respondText(errorResponse, ContentType.Application.Json, HttpStatusCode.InternalServerError)
         }
     }
 }
